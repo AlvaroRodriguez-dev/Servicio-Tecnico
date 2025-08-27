a6ffbff658b26c6fe238f6b02242748ae9197070
Alvaro Rodriguez Aramayo <arodriguez@FABOCE.LOC>
2025-08-27 14:53:28 -0400

    Teminado CRUD de equipos, clientes ordenes usuarios y equipos. ademas de estilos para vistas de clientes index, create y edit.

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -1,11 +1,47 @@
 <x-app-layout>
-    <x-slot name="header">Editar Cliente</x-slot>
-    <form method="POST" action="{{ route('clientes.update', $cliente) }}">
-        @csrf @method('PUT')
-        <input name="nombre" value="{{ $cliente->nombre }}">
-        <input name="email" value="{{ $cliente->email }}">
-        <input name="telefono" value="{{ $cliente->telefono }}">
-        <button type="submit">Actualizar</button>
-    </form>
-    <a href="{{ route('clientes.index') }}">Volver</a>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Editar Cliente') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    <form method="POST" action="{{ route('clientes.update', $cliente) }}" class="mt-6 space-y-6">
+                        @csrf @method('PUT')
+                        <div>
+                            <x-input-label for="nombre" :value="__('Nombre')" />
+                            <x-text-input id="nombre" name="nombre" type="text" class="mt-1 block w-full" :value="old('nombre', $cliente->nombre)" required autofocus autocomplete="nombre" />
+                            <x-input-error class="mt-2" :messages="$errors->get('nombre')" />
+                        </div>
+
+                        <div>
+                            <x-input-label for="email" :value="__('Correo Electronico')" />
+                            <x-text-input id="email" name="email" type="text" class="mt-1 block w-full" :value="old('email', $cliente->email)" required autofocus autocomplete="email" />
+                            <x-input-error class="mt-2" :messages="$errors->get('email')" />
+                        </div>
+
+                        <div>
+                            <x-input-label for="telefono" :value="__('Telefono')" />
+                            <x-text-input id="telefono" name="telefono" type="text" class="mt-1 block w-full" :value="old('telefono', $cliente->telefono)" required autofocus autocomplete="telefono" />
+                            <x-input-error class="mt-2" :messages="$errors->get('telefono')" />
+                        </div>
+
+                        <div class="flex items-center gap-4">
+                            <x-primary-button>{{ __('Actualizar') }}</x-primary-button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    <a href="{{ route('clientes.index') }}">Volver</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </x-app-layout>
